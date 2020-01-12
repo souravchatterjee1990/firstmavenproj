24108e5731c11ea8dbb975dbc16c745cdeb1727c
Sourav Chatterjee <souravchatterjee1990@hotmail.com>
2020-01-12 08:41:25 +0530

    commiting my changes to local git repository

---
+++ com.sourav.projects/src/main/java/com/sourav/projects/Greeter.java
@@ -0,0 +1,15 @@
+package com.sourav.projects;
+
+public class Greeter {
+	/**
+	   * This is a constructor.
+	   */
+	  public Greeter() {
+
+	  }
+
+	  //TODO: Add javadoc comment
+	  public String greet(String someone) {
+	    return String.format("Hello, %s!", someone);
+	  }
+}
